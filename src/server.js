7d80bda81ef3d967a0553e3e95ade221dac39d1c
KseniiaMazan <ksusamazan@gmail.com>
2019-11-12 16:59:53 +0200

    comments are fixed and extra tasks are added

--- src/server.js
+++ src/server.js
@@ -2,20 +2,22 @@
 
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
+const uuid = require('uuid/v4');
 
 const port = 5000;
 const app = express();
 
-const movies = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+let movies = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 
 app.get('/movies', (request, response) => {
-  const dataToSend = movies.sort((a,b) => a.year - b.year);
+  const dataToSend = [...movies].sort((a,b) => a.year - b.year);
 
   response.json(dataToSend);
 });
 
 app.get('/movies/titles', (req, res) => {
-  let titles = movies;
+  let titles = [...movies];
 
   if (req.query.year) {
     titles = titles.filter(({year}) => year === req.query.year);
@@ -26,9 +28,8 @@ app.get('/movies/titles', (req, res) => {
 
   titles = titles
       .sort((a, b) => a.title.localeCompare(b.title))
-      .reduce((acc, item) => (
-          acc = `${acc}\n${item.title}`
-      ), '');
+      .map(movie => movie.title)
+      .join('\n');
 
   res.send(titles);
 });
@@ -41,6 +42,75 @@ app.get('/movies/:movieId', (request, response) => {
   } else {
     response.status(404).send("Oops, something went wrong");
   }
+});
+
+app.post('/movies', bodyParser.json(), (req, res) => {
+  const newFilm = {
+    id: uuid(),
+    title: req.body.title,
+    year: req.body.year,
+    imdbRating: req.body.imdbRating,
+  };
+
+  movies.push(newFilm);
+  fs.writeFile('data.json', JSON.stringify(movies), (err) => {
+    if (err) throw err;
+  });
+
+  res.json(newFilm);
+});
+
+app.put('/movies/:movieId',  bodyParser.json(), (req, res) => {
+  let updatedFilm = movies.find(({ id }) => id === req.params.movieId);
+
+  if(!updatedFilm) {
+    res.status(404).send("Film is not found(");
+  };
+
+  updatedFilm = {
+    ...updatedFilm,
+    title: req.body.title,
+    year: req.body.year,
+    imdbRating: req.body.imdbRating,
+  };
+
+  movies = [...movies, updatedFilm];
+
+  fs.writeFile('data.json', JSON.stringify(movies), (err) => {
+    if (err) throw err;
+  });
+
+  res.json(updatedFilm);
+});
+
+app.patch('/movies/:movieId', bodyParser.json(), (req, res) => {
+  let updatedFilm = movies.find(({ id }) => id === req.params.movieId);
+
+  if(!updatedFilm) {
+    res.status(404).send("Film is not found(");
+  };
+
+  updatedFilm = {
+    ...updatedFilm,
+    ...req.body,
+  };
+
+  movies = [...movies, updatedFilm];
+
+  fs.writeFile('data.json', JSON.stringify(movies), (err) => {
+    if (err) throw err;
+  });
+
+  res.json(updatedFilm);
+});
+
+app.delete('/movies/:movieId', (req, res) => {
+  movies.filter(({ id }) => id !== req.params.movieId);
+  fs.writeFile('data.json', JSON.stringify(movies), (err) => {
+    if (err) throw err;
+  });
+
+  res.sendStatus(204);
 });
 
 app.listen(port, () => {
